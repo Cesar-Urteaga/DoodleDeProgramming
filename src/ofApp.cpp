451c4625152faa83174e93d4699342738712faa8
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-02-07 19:14:51 +0900

    Twin Circle. https://junkiyoshi.com/openframeworks20260207/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,8 +6,8 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
-	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_SUBTRACT);
+	ofBackground(39);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
 	ofNoFill();
 }
 
@@ -18,10 +18,10 @@ void ofApp::update() {
 	for (int i = 0; i < 20; i++) {
 
 		auto deg = ofRandom(360);
-		auto start_radius = ofRandom(2) < 1 ? 190 : 310;
+		auto start_radius = ofRandom(2) < 1 ? 180 : 320;
 		auto location = glm::vec2(start_radius * cos(deg * DEG_TO_RAD), start_radius * sin(deg * DEG_TO_RAD));
 
-		color.setHsb(start_radius == 190 ? 0 : 128, 255, 255);
+		color.setHsb(start_radius == 180 ? ofRandom(60, 80) : ofRandom(190, 210), 200, 255);
 
 		vector<glm::vec2> log;
 		log.push_back(location);
@@ -48,7 +48,7 @@ void ofApp::update() {
 
 			auto deg = ofMap(ofNoise(glm::vec3(this->log_list[i].back() * 0.01, count * 0.01 + ofGetFrameNum() * 0.01)), 0, 1, -360, 360);
 			auto tmp = this->log_list[i].back() + glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
-			if (glm::length(tmp) > 190 && glm::length(tmp) < 310) {
+			if (glm::length(tmp) > 180 && glm::length(tmp) < 320) {
 
 				this->log_list[i].push_back(this->log_list[i].back() + glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD)));
 				break;
@@ -58,7 +58,8 @@ void ofApp::update() {
 				count++;
 			}
 		}
-		while (this->log_list[i].size() > 600) {
+
+		while (this->log_list[i].size() > 100) {
 
 			this->log_list[i].erase(this->log_list[i].begin());
 		}
@@ -68,27 +69,33 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-	ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
-	ofRotateZ(90);
+	ofTranslate(ofGetWindowSize() * 0.5);
 
 	for (int i = 0; i < this->log_list.size(); i++) {
+
+		ofPushMatrix();
+
+		int t = this->color_list[i].getHue();
+		t >= 190 && t <= 210 ? ofRotateZ(ofGetFrameNum() * 0.36) : ofRotateZ(ofGetFrameNum() * -0.36);
 
 		if (this->life_list[i] > 80) {
 
 			ofSetColor(this->color_list[i]);
-			ofSetLineWidth(2);
+			ofSetLineWidth(1);
 		}
 		else {
 
 			ofSetColor(ofColor(this->color_list[i], ofMap(this->life_list[i], 0, 80, 0, 255)));
-			ofSetLineWidth(ofMap(this->life_list[i], 0, 80, 0, 2));
+			ofSetLineWidth(ofMap(this->life_list[i], 0, 80, 0, 1));
 		}
 
 		ofBeginShape();
 		ofVertices(this->log_list[i]);
 		ofEndShape();
+
+		ofPopMatrix();
 	}
-	
+
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
 	int start = 500;
