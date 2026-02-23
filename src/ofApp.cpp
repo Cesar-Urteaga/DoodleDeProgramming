e2106e6113b077f9ac926cba7b39847c0624ab17
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-02-23 19:03:24 +0900

    Rainbow Shower. https://junkiyoshi.com/openframeworks20260223/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,14 +6,12 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openframeworks");
 
-	ofBackground(239);
-	ofNoFill();
-	ofSetLineWidth(3);
+	ofBackground(39);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-
 
 }
 
@@ -25,57 +23,16 @@ void ofApp::draw() {
 	ofSeedRandom(39);
 	auto noise_seed = glm::vec2(ofRandom(1000), ofRandom(1000));
 
-	int count = 60;
-	int len = 120;
-	float head_size = 3;
+	int count = 25;
+	int len = 160;
+	float head_size = 2;
 	
-	ofSetColor(39);
+	ofColor color;
+
 	for (int k = 0; k < count; k++) {
 
-		vector<glm::vec3> right, left, frame;
-
-		glm::vec3 last_location;
-		float last_theta;
-
-		float random = ofRandom(1000);
-		float deg = ofMap(k, 0, count, 0, 360);
-		float span = ofRandom(0.01, 0.08);
-		for (int i = 0; i < len; i++) {
-
-			auto radius = ofMap(sin((random + ofGetFrameNum() + i) * span), 0, 1, 270, 330);
-			auto next_radius = ofMap(sin((random + ofGetFrameNum() + i + 1) * span), 0, 1, 270, 330);
-
-			auto location = glm::vec3(radius * cos((ofGetFrameNum() + i + deg) * DEG_TO_RAD), radius * sin((ofGetFrameNum() + i + deg) * DEG_TO_RAD), 0);
-			auto next = glm::vec3(next_radius * cos((ofGetFrameNum() + i + 1 + deg) * DEG_TO_RAD), next_radius * sin((ofGetFrameNum() + i + 1 + deg) * DEG_TO_RAD), 0);
-
-			auto direction = next - location;
-			auto theta = atan2(direction.y, direction.x);
-
-			right.push_back(location + glm::vec3(ofMap(i, 0, len, 0, head_size) * cos(theta + PI * 0.5), ofMap(i, 0, len, 0, head_size) * sin(theta + PI * 0.5), 0));
-			left.push_back(location + glm::vec3(ofMap(i, 0, len, 0, head_size) * cos(theta - PI * 0.5), ofMap(i, 0, len, 0, head_size) * sin(theta - PI * 0.5), 0));
-
-			last_location = location;
-			last_theta = theta;
-		}
-
-		for (auto theta = last_theta - PI * 0.5; theta <= last_theta + PI * 0.5; theta += PI / 20) {
-
-			frame.push_back(last_location + glm::vec3(head_size * cos(theta), head_size * sin(theta), 0));
-		}
-
-		ofBeginShape();
-		ofVertices(frame);
-		std::reverse(right.begin(), right.end());
-		ofVertices(right);
-		ofVertices(left);
-		ofEndShape(true);
-	}
-
-	ofSeedRandom(39);
-	noise_seed = glm::vec2(ofRandom(1000), ofRandom(1000));
-
-	ofSetColor(239);
-	for (int k = 0; k < count; k++) {
+		color.setHsb(ofMap(k, 0, count, 0, 255), 130, 255);
+		ofSetColor(color);
 
 		ofMesh mesh;
 		vector<glm::vec3> right, left;
@@ -83,13 +40,12 @@ void ofApp::draw() {
 		glm::vec3 last_location;
 		float last_theta;
 
-		float random = ofRandom(1000);
 		float deg = ofMap(k, 0, count, 0, 360);
-		float span = ofRandom(0.01, 0.08);
+		float span = 0.0314;
 		for (int i = 0; i < len; i++) {
 
-			auto radius = ofMap(sin((random + ofGetFrameNum() + i) * span), 0, 1, 270, 330);
-			auto next_radius = ofMap(sin((random + ofGetFrameNum() + i + 1) * span), 0, 1, 270, 330);
+			auto radius = ofMap(sin((ofGetFrameNum() + i) * span), 0, 1, 270, 330);
+			auto next_radius = ofMap(sin((ofGetFrameNum() + i + 1) * span), 0, 1, 270, 330);
 
 			auto location = glm::vec3(radius * cos((ofGetFrameNum() + i + deg) * DEG_TO_RAD), radius * sin((ofGetFrameNum() + i + deg) * DEG_TO_RAD), 0);
 			auto next = glm::vec3(next_radius * cos((ofGetFrameNum() + i + 1 + deg) * DEG_TO_RAD), next_radius * sin((ofGetFrameNum() + i + 1 + deg) * DEG_TO_RAD), 0);
@@ -109,7 +65,6 @@ void ofApp::draw() {
 			mesh.addVertex(left[i]);
 			mesh.addVertex(right[i]);
 		}
-
 
 		for (int i = 0; i < mesh.getNumVertices() - 2; i += 2) {
 
@@ -134,7 +89,7 @@ void ofApp::draw() {
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
-	int start = 500;
+	int start = 60;
 	if (ofGetFrameNum() > start) {
 
 		std::ostringstream os;
