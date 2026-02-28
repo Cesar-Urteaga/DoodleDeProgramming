02b5a87a6680501ba1eb4ac617a8cbc907b3668a
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-02-28 21:28:21 +0900

    I saw Super Princess Kaguya at the theater. https://junkiyoshi.com/openframeworks20260228/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,80 +6,47 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(39);
-	ofSetLineWidth(0.5);
-	ofEnableDepthTest();
-
-	this->mesh.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
+	ofBackground(239);
+	ofSetColor(39);
+	ofSetRectMode(ofRectMode::OF_RECTMODE_CENTER);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 
-	ofSeedRandom(39);
-
-	vector<float> min_distance_list;
-	this->mesh.clear();
-
-	for (int i = 0; i < 1200; i++) {
-
-		auto vertex = glm::vec3(
-			ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.001), 0, 1, -160, 160),
-			ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.001), 0, 1, -160, 160),
-			ofMap(ofNoise(ofRandom(1000), ofGetFrameNum() * 0.001), 0, 1, -160, 160));
-		this->mesh.addVertex(vertex);
-		min_distance_list.push_back(40);
-	}
-
-	for (auto& vertex : this->mesh.getVertices()) {
-
-		auto len = glm::length(vertex);
-		if (len < 150) {
-
-			vertex = glm::normalize(vertex) * 150;
-		}
-	}
-
-
-	for (int i = 0; i < this->mesh.getNumVertices(); i++) {
-
-		for (int k = 0; k < this->mesh.getNumVertices(); k++) {
-
-			if (i == k) { continue; }
-
-			auto distance = glm::distance(this->mesh.getVertex(i), this->mesh.getVertex(k));
-			if (distance < 30) {
-
-				this->mesh.addIndex(i);
-				this->mesh.addIndex(k);
-
-				if (distance < min_distance_list[i]) {
-
-					min_distance_list[i] = distance;
-				}
-			}
-		}
-
-		this->mesh.addColor(ofColor(255, ofMap(min_distance_list[i], 0, 30, 255, 0)));
-	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-	this->cam.begin();
+	int span = 60;
+	int param = ofGetFrameNum() % 50;
+	int deg = param < 25 ? 0 : ofMap(param, 25, 50, 0, 90);
 
-	ofRotateY(ofGetFrameNum() * 0.72);
+	for (int y = span * 0.5; y < ofGetWindowHeight(); y += span * 2) {
 
-	this->mesh.draw();
+		for (int x = span * 0.5; x < ofGetWindowWidth(); x += span * 2) {
 
-	for (int i = 0; i < this->mesh.getNumVertices(); i++) {
+			ofPushMatrix();
+			ofTranslate(x, y);
 
-		ofSetColor(this->mesh.getColor(i));
-		ofDrawSphere(this->mesh.getVertex(i), 1.5);
+			ofRotate(deg);
+
+			ofDrawRectangle(glm::vec2(), span, span);
+
+			ofPopMatrix();
+
+			ofPushMatrix();
+			ofTranslate(x + span, y + span);
+
+			ofRotate(deg);
+
+			ofDrawRectangle(glm::vec2(), span, span);
+
+			ofPopMatrix();
+
+		}
 	}
-
-	this->cam.end();
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
--- src/ofApp.h
+++ src/ofApp.h
@@ -17,7 +17,4 @@ public:
 	void windowResized(int w, int h) {};
 	void dragEvent(ofDragInfo dragInfo) {};
 	void gotMessage(ofMessage msg) {};
-
-	ofEasyCam cam;
-	ofMesh mesh;
 };
