bcd80777d55050501b3c64732ed1a863b711632c
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-03-13 20:38:01 +0900

    Drunkard's hallucination. https://junkiyoshi.com/openframeworks20260313/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,35 +6,47 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
+	ofBackground(39);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 
+	ofSeedRandom(39);
+
 	this->mesh.clear();
 
 	float span = 0.5;
-	for (float noise_param = 0; noise_param < 1.1; noise_param += 0.1) {
+	ofColor color;
+	auto noise_seed = ofRandom(1000);
 
-		for (float x = 20; x <= 700; x += span) {
+	for (int i = 0; i < 5; i++) {
 
-			for (float y = 20; y <= 700; y += span) {
+		noise_seed += 0.02;
+		color.setHsb(ofMap(i, 0, 5, 0, 255), 255, 255);
 
-				float noise_value = ofNoise(int(x / 20) * 20 * 0.0025, int(y / 20) * 20 * 0.0025, ofGetFrameNum() * 0.0005);
-				if (noise_value >= noise_param - 0.025 && noise_value <= noise_param + 0.025) {
+		for (float noise_param = 0; noise_param < 1.1; noise_param += 0.1) {
 
-					this->mesh.addVertex(glm::vec3(x, y, 0));
-					this->mesh.addVertex(glm::vec3(x + span, y, 0));
-					this->mesh.addVertex(glm::vec3(x + span, y + span, 0));
-					this->mesh.addVertex(glm::vec3(x, y + span, 0));
+			for (float x = 20; x <= 700; x += span) {
 
-					this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 4); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
-					this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 2); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
+				for (float y = 20; y <= 700; y += span) {
 
-					for (int i = 0; i < 4; i++) {
+					float noise_value = ofNoise(noise_seed, x * 0.003, y * 0.003, ofGetFrameNum() * 0.01);
+					if (noise_value >= noise_param - 0.01 && noise_value <= noise_param + 0.01) {
 
-						this->mesh.addColor(ofColor(39));
+						this->mesh.addVertex(glm::vec3(x, y, 0));
+						this->mesh.addVertex(glm::vec3(x + span, y, 0));
+						this->mesh.addVertex(glm::vec3(x + span, y + span, 0));
+						this->mesh.addVertex(glm::vec3(x, y + span, 0));
+
+						this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 4); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
+						this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 2); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
+
+						for (int i = 0; i < 4; i++) {
+
+							this->mesh.addColor(ofColor(color, 64));
+						}
 					}
 				}
 			}
