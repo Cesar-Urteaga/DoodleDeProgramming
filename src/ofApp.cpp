a82f425ffbb6f60ce98a4f5651c1fb90fb9012c9
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-03-22 20:15:07 +0900

    Double circle. https://junkiyoshi.com/openframeworks20260323/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,8 +6,9 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
-	ofSetColor(39);
+	ofBackground(39);
+	ofSetColor(239);
+	ofNoFill();
 	ofEnableDepthTest();
 }
 
@@ -29,22 +30,32 @@ void ofApp::draw() {
 	float noise_seed_x = ofRandom(1000);
 	float noise_seed_y = ofRandom(1000);
 	float noise_seed_z = ofRandom(1000);
+
+	vector<glm::vec3> vertices_1, vertices_2;
+
 	for (int deg = 0; deg < 360; deg += 8) {
 
 		auto location_1 = glm::vec3(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD), 0);
-		auto location_2 = glm::vec3(radius * 0.3 * cos(deg * DEG_TO_RAD), radius * 0.3 * sin(deg * DEG_TO_RAD), 0);
 
-		auto rotation_z = glm::rotate(glm::mat4(), ofMap(ofNoise(noise_seed_z, location_1.x * 0.0003, location_1.y * 0.0003, ofGetFrameNum() * 0.005), 0, 1, -PI * 2, PI * 2), glm::vec3(0, 0, 1));
-		auto rotation_y = glm::rotate(glm::mat4(), ofMap(ofNoise(noise_seed_y, location_1.x * 0.0003, location_1.y * 0.0003, ofGetFrameNum() * 0.005), 0, 1, -PI * 2, PI * 2), glm::vec3(0, 1, 0));
-		auto rotation_x = glm::rotate(glm::mat4(), ofMap(ofNoise(noise_seed_x, location_1.x * 0.0003, location_1.x * 0.0003, ofGetFrameNum() * 0.005), 0, 1, -PI * 2, PI * 2), glm::vec3(1, 0, 0));
+		auto rotation_z = glm::rotate(glm::mat4(), ofMap(ofNoise(noise_seed_z, location_1.x * 0.0001, location_1.y * 0.0001, ofGetFrameNum() * 0.01), 0, 1, -PI * 1, PI * 1), glm::vec3(0, 0, 1));
+		auto rotation_y = glm::rotate(glm::mat4(), ofMap(ofNoise(noise_seed_y, location_1.x * 0.0001, location_1.y * 0.0001, ofGetFrameNum() * 0.01), 0, 1, -PI * 1, PI * 1), glm::vec3(0, 1, 0));
+		auto rotation_x = glm::rotate(glm::mat4(), ofMap(ofNoise(noise_seed_x, location_1.x * 0.0001, location_1.x * 0.0001, ofGetFrameNum() * 0.01), 0, 1, -PI * 1, PI * 1), glm::vec3(1, 0, 0));
 
-		location_2 = glm::vec4(location_2, 0) * rotation_y * rotation_x;
+		auto location_2 = glm::vec4(location_1, 0) * rotation_y * rotation_x;
 
 		ofDrawLine(location_1, location_2);
 
-		ofDrawSphere(location_1, 3);
-		ofDrawSphere(location_2, 2);
+		vertices_1.push_back(location_1);
+		vertices_2.push_back(location_2);
 	}
+
+	ofBeginShape();
+	ofVertices(vertices_1);
+	ofEndShape(true);
+
+	ofBeginShape();
+	ofVertices(vertices_2);
+	ofEndShape(true);
 
 	this->cam.end();
 
