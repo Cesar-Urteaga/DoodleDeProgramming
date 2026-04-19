fabef865b90d36723c6c059095cf2831d345794a
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-04-19 20:47:00 +0900

    Bones of torus. https://junkiyoshi.com/openframeworks20260419/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,45 +4,78 @@
 void ofApp::setup() {
 
 	ofSetFrameRate(25);
-	ofSetWindowTitle("openFrameworks");
+	ofSetWindowTitle("openframeworks");
 
-	ofBackground(39);
-	ofSetLineWidth(1);
+	ofBackground(239);
 	ofEnableDepthTest();
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
+
+	ofSeedRandom(39);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
 
 	this->cam.begin();
-	ofRotateX(90);
 
-	auto radius = 300;
+	int v_span = 30;
+	int u_span = 10;
+	int R = 200;
 
-	for (auto deg = 0; deg < 360; deg += 4) {
+	ofMesh face, line;
+	line.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
 
-		auto location = glm::vec3(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD), 0);
-		auto depth = 300;
+	float noise_seed = ofRandom(1000);
+	for (int v = 0; v <= 360; v += v_span) {
 
-		ofPushMatrix();
-		ofTranslate(location);
-		ofRotateZ(deg);
-		ofRotateY(deg * 4 + ofGetFrameNum() * 4);
+		int u_start = ofMap(ofNoise(noise_seed, cos(v * DEG_TO_RAD) * 0.25, sin(v * DEG_TO_RAD) * 0.25, ofGetFrameNum() * 0.01), 0, 1, -720, 720);
+		int next_u = ofMap(ofNoise(noise_seed, cos((v + v_span * 0.9) * DEG_TO_RAD) * 0.25, sin((v + v_span * 0.9) * DEG_TO_RAD) * 0.25, ofGetFrameNum() * 0.01), 0, 1, -720, 720);
+		int r = ofMap(ofNoise(glm::vec4(this->make_point(R, 30, u_start, v) * 0.01, ofGetFrameNum() * 0.005)), 0, 1, 0, 150);
 
-		ofFill();
-		ofSetColor(0);
-		ofDrawBox(45, 2, depth);
+		for (int u = u_start; u < u_start + 360; u += u_span) {
 
-		ofNoFill();
-		ofSetColor(239);
-		ofDrawBox(45, 2, depth);
+			face.addVertex(this->make_point(R, r, u, v));
+			face.addVertex(this->make_point(R, r, u + u_span, v));
+			face.addVertex(this->make_point(R, r, next_u + u_span, v + v_span * 0.8));
+			face.addVertex(this->make_point(R, r, next_u, v + v_span * 0.8));
 
-		ofPopMatrix();
+			line.addVertex(this->make_point(R, r, u, v));
+			line.addVertex(this->make_point(R, r, u + u_span, v));
+			line.addVertex(this->make_point(R, r, next_u + u_span, v + v_span * 0.8));
+			line.addVertex(this->make_point(R, r, next_u, v + v_span * 0.8));
+
+			ofColor face_color = ofColor(39);
+			ofColor line_color = ofColor(239);
+
+			face.addColor(face_color);
+			face.addColor(face_color);
+			face.addColor(face_color);
+			face.addColor(face_color);
+
+			line.addColor(line_color);
+			line.addColor(line_color);
+			line.addColor(line_color);
+			line.addColor(line_color);
+
+			face.addIndex(face.getNumVertices() - 1); face.addIndex(face.getNumVertices() - 2); face.addIndex(face.getNumVertices() - 3);
+			face.addIndex(face.getNumVertices() - 1); face.addIndex(face.getNumVertices() - 3); face.addIndex(face.getNumVertices() - 4);
+
+			//line.addIndex(line.getNumVertices() - 1); line.addIndex(line.getNumVertices() - 4);
+			//line.addIndex(line.getNumVertices() - 2); line.addIndex(line.getNumVertices() - 3);
+
+			line.addIndex(line.getNumVertices() - 1); line.addIndex(line.getNumVertices() - 2);
+			line.addIndex(line.getNumVertices() - 3); line.addIndex(line.getNumVertices() - 4);
+
+			next_u += u_span;
+
+		}
 	}
+
+	face.drawFaces();
+	line.drawWireframe();
 
 	this->cam.end();
 
@@ -65,7 +98,23 @@ void ofApp::draw() {
 }
 
 //--------------------------------------------------------------
+glm::vec3 ofApp::make_point(float R, float r, float u, float v) {
+
+	// 数学デッサン教室 描いて楽しむ数学たち　P.31
+
+	u *= DEG_TO_RAD;
+	v *= DEG_TO_RAD;
+
+	auto x = (R + r * cos(u)) * cos(v);
+	auto y = (R + r * cos(u)) * sin(v);
+	auto z = r * sin(u);
+
+	return glm::vec3(x, y, z);
+}
+
+//--------------------------------------------------------------
 int main() {
+
 	ofSetupOpenGL(720, 720, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
