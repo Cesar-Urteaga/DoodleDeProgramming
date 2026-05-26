3db027a33a950084887a30be82f3b366bd8c7c0e
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-05-26 21:30:13 +0900

    Rainbow Flower. https://junkiyoshi.com/openframeworks20260526/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,8 +6,9 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openframeworks");
 
-	ofBackground(239);
+	ofBackground(39);
 	ofSetLineWidth(2);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
 }
 
 //--------------------------------------------------------------
@@ -22,26 +23,29 @@ void ofApp::draw() {
 
 	int radius = 100;
 
+	ofColor color;
 	for (int i = 0; i < 5; i++) {
 
 		vector<glm::vec2> vertices;
 		for (float deg = 0; deg < 360; deg += 0.3) {
 
 			auto location = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
-			auto noise_value = ofNoise(glm::vec4(i, location * 0.05, ofGetFrameNum() * 0.005));
+			auto noise_value = ofNoise(glm::vec4(i * 0.2, location * 0.1, ofGetFrameNum() * 0.005));
 			location *= noise_value > 0.5 ? ofMap(noise_value, 0.5, 1, 0.5, 4) : 0.5;
 
 			vertices.push_back(location);
 		}
 
+		color.setHsb(ofMap(i, 0, 5, 0, 255), 255, 255);
+
 		ofFill();
-		ofSetColor(239);
+		ofSetColor(color, 64);
 		ofBeginShape();
 		ofVertices(vertices);
 		ofEndShape(true);
 
 		ofNoFill();
-		ofSetColor(39, ofMap(i, 0, 5, 32, 255));
+		ofSetColor(color);
 		ofBeginShape();
 		ofVertices(vertices);
 		ofEndShape(true);
