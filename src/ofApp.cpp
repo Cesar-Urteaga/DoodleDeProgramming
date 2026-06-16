96339906ab7a345161027369838ab9a2223acdf3
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-06-16 19:26:17 +0900

    Mosaic cylinder. https://junkiyoshi.com/openframeworks20260616/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,61 +4,136 @@
 void ofApp::setup() {
 
 	ofSetFrameRate(25);
-	ofSetWindowTitle("openframeworks");
+	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(39);
-	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
+	ofBackground(239);
+	ofSetLineWidth(2);
+	ofEnableDepthTest();
+
+	this->line.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 
+	this->noise_param += 0.01;
+
+	this->face.clear();
+	this->line.clear();
+
+	float threshold_1 = 0.4;
+	float threshold_2 = 0.6;
+	float deg_span = 5;
+	float z_span = 25;
+	float noise_span = 0.015;
+	float noise_span_z_scale = 1;
+	float radius = 320;
+
+	for (float deg = 0; deg < 360; deg += deg_span) {
+
+		for (float z = -500 + z_span; z <= 500 - z_span; z += z_span) {
+
+			auto noise_value = ofNoise(glm::vec4(radius * cos(deg * DEG_TO_RAD) * noise_span, radius * sin(deg * DEG_TO_RAD) * noise_span, z * noise_span * noise_span_z_scale, noise_param));
+			noise_value = abs(z) > 470 ? 0.5 : noise_value;
+			if (noise_value <= threshold_1 || noise_value >= threshold_2) { continue; }
+
+			auto noise_1 = ofNoise(glm::vec4(radius * cos((deg - deg_span) * DEG_TO_RAD) * noise_span, radius * sin((deg - deg_span) * DEG_TO_RAD) * noise_span, z * noise_span * noise_span_z_scale, noise_param));
+			auto noise_2 = ofNoise(glm::vec4(radius * cos(deg * DEG_TO_RAD) * noise_span, radius * sin(deg * DEG_TO_RAD) * noise_span, (z + z_span) * noise_span * noise_span_z_scale, noise_param));
+			auto noise_3 = ofNoise(glm::vec4(radius * cos(deg * DEG_TO_RAD) * noise_span, radius * sin(deg * DEG_TO_RAD) * noise_span, (z - z_span) * noise_span * noise_span_z_scale, noise_param));
+			auto noise_4 = ofNoise(glm::vec4(radius * cos((deg + deg_span) * DEG_TO_RAD) * noise_span, radius * sin((deg + deg_span) * DEG_TO_RAD) * noise_span, z * noise_span * noise_span_z_scale, noise_param));
+
+			noise_1 = abs(z) > 470 ? 0.5 : noise_1;
+			noise_2 = abs(z + z_span) > 470 ? 0.5 : noise_2;
+			noise_3 = abs(z - z_span) > 470 ? 0.5 : noise_3;
+			noise_4 = abs(z) > 470 ? 0.5 : noise_4;
+
+			auto index = this->face.getNumVertices();
+			vector<glm::vec3> vertices;
+
+			vertices.push_back(glm::vec3(radius * cos((deg - deg_span * 0.5) * DEG_TO_RAD), radius * sin((deg - deg_span * 0.5) * DEG_TO_RAD), z - z_span * 0.5));
+			vertices.push_back(glm::vec3(radius * cos((deg + deg_span * 0.5) * DEG_TO_RAD), radius * sin((deg + deg_span * 0.5) * DEG_TO_RAD), z - z_span * 0.5));
+			vertices.push_back(glm::vec3(radius * cos((deg - deg_span * 0.5) * DEG_TO_RAD), radius * sin((deg - deg_span * 0.5) * DEG_TO_RAD), z + z_span * 0.5));
+			vertices.push_back(glm::vec3(radius * cos((deg + deg_span * 0.5) * DEG_TO_RAD), radius * sin((deg + deg_span * 0.5) * DEG_TO_RAD), z + z_span * 0.5));
+
+			this->face.addVertices(vertices);
+
+			this->face.addIndex(index + 0); this->face.addIndex(index + 1); this->face.addIndex(index + 3);
+			this->face.addIndex(index + 0); this->face.addIndex(index + 2); this->face.addIndex(index + 3);
+
+			ofColor face_color(0);
+			for (int i = 0; i < 4; i++) {
+
+				this->face.addColor(face_color);
+			}
+
+			ofColor line_color(255);
+			if (noise_1 <= threshold_1 || noise_1 >= threshold_2) {
+
+				this->line.addVertex(vertices[0]);
+				this->line.addVertex(vertices[2]);
+
+				this->line.addIndex(this->line.getNumVertices() - 1);
+				this->line.addIndex(this->line.getNumVertices() - 2);
+
+				this->line.addColor(line_color);
+				this->line.addColor(line_color);
+			}
+
+			if (noise_2 <= threshold_1 || noise_2 >= threshold_2) {
+
+				this->line.addVertex(vertices[2]);
+				this->line.addVertex(vertices[3]);
+
+				this->line.addIndex(this->line.getNumVertices() - 1);
+				this->line.addIndex(this->line.getNumVertices() - 2);
+
+				this->line.addColor(line_color);
+				this->line.addColor(line_color);
+			}
+
+			if (noise_3 <= threshold_1 || noise_3 >= threshold_2) {
+
+				this->line.addVertex(vertices[0]);
+				this->line.addVertex(vertices[1]);
+
+				this->line.addIndex(this->line.getNumVertices() - 1);
+				this->line.addIndex(this->line.getNumVertices() - 2);
+
+				this->line.addColor(line_color);
+				this->line.addColor(line_color);
+			}
+
+			if (noise_4 <= threshold_1 || noise_4 >= threshold_2) {
+
+				this->line.addVertex(vertices[1]);
+				this->line.addVertex(vertices[3]);
+
+				this->line.addIndex(this->line.getNumVertices() - 1);
+				this->line.addIndex(this->line.getNumVertices() - 2);
+
+				this->line.addColor(line_color);
+				this->line.addColor(line_color);
+			}
+		}
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-	ofTranslate(ofGetWindowSize() * 0.5);
+	this->cam.begin();
+	this->cam.setPosition(0, 0, 1200);
+	ofRotateX(90);
+	ofRotateZ(ofGetFrameNum() * 0.72);
+	
+	this->line.draw();
+	this->face.draw();
 
-	int deg_span = 30;
-	int radius_span = 1;
-	int len = 2;
-	ofColor color;
-	for (int deg = 0; deg < 360; deg += deg_span) {
-
-		for (int radius = 0; radius < 720; radius += radius_span) {
-
-			color.setHsb(ofGetFrameNum() % 255, 200, 255);
-			ofSetColor(color);
-
-			auto noise_deg = ofMap(ofNoise(radius * 0.00025 - ofGetFrameNum() * 0.0025), 0, 1, 180, -180);
-			auto next_noise_deg = ofMap(ofNoise((radius + radius_span) * 0.00025 - ofGetFrameNum() * 0.0025), 0, 1, 180, -180);
-
-			ofBeginShape();
-			ofVertex(glm::vec2(radius * cos((deg + noise_deg - len) * DEG_TO_RAD), radius * sin((deg + noise_deg - len) * DEG_TO_RAD)));
-			ofVertex(glm::vec2(radius * cos((deg + noise_deg + len) * DEG_TO_RAD), radius * sin((deg + noise_deg + len) * DEG_TO_RAD)));
-			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg + len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg + len) * DEG_TO_RAD)));
-			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg - len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg - len) * DEG_TO_RAD)));
-			ofEndShape(true);
-
-			color.setHsb((ofGetFrameNum() + 128) % 255, 200, 255);
-			ofSetColor(color);
-
-			noise_deg = ofMap(ofNoise(radius * 0.00025 - ofGetFrameNum() * 0.0025), 0, 1, -180, 180);
-			next_noise_deg = ofMap(ofNoise((radius + radius_span) * 0.00025 - ofGetFrameNum() * 0.0025), 0, 1, -180, 180);
-
-			ofBeginShape();
-			ofVertex(glm::vec2(radius * cos((deg + noise_deg - len) * DEG_TO_RAD), radius * sin((deg + noise_deg - len) * DEG_TO_RAD)));
-			ofVertex(glm::vec2(radius * cos((deg + noise_deg + len) * DEG_TO_RAD), radius * sin((deg + noise_deg + len) * DEG_TO_RAD)));
-			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg + len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg + len) * DEG_TO_RAD)));
-			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg - len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg - len) * DEG_TO_RAD)));
-			ofEndShape(true);
-		}
-	}
+	this->cam.end();
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
-	int start = 10;
+	int start = 2;
 	if (ofGetFrameNum() > start) {
 
 		std::ostringstream os;
--- src/ofApp.h
+++ src/ofApp.h
@@ -17,4 +17,9 @@ public:
 	void windowResized(int w, int h) {};
 	void dragEvent(ofDragInfo dragInfo) {};
 	void gotMessage(ofMessage msg) {};
+
+	ofEasyCam cam;
+	ofMesh face, line;
+
+	float noise_param;
 };
