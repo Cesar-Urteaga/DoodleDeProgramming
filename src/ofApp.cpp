dcf6407e583ff18e9fa0548542821f6d1308b548
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-06-20 21:37:49 +0900

    Difference in speed. https://junkiyoshi.com/openframeworks20260620/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,110 +6,44 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(39);
-	ofSetLineWidth(2);
-	ofEnableDepthTest();
-
-	this->line.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
+	ofBackground(239);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 
-	ofSeedRandom(39);
+	this->mesh.clear();
 
-	auto seed = glm::vec3(ofRandom(1000), ofRandom(1000), ofRandom(1000));
+	float span = 0.5;
 	ofColor color;
+	bool flag = true;
+	for (float x_start = 0; x_start < 720; x_start += 144)
+	{
+		flag = !flag;
 
-	this->face.clear();
-	this->line.clear();
+		for (float x = x_start + 7; x < x_start + 137; x += span) {
 
-	float phi_deg_step = 4;
-	float theta_deg_step = 4;
-	float threshold_1 = 0.5;
-	float threshold_2 = 0.6;
+			for (float y = 0; y < 720; y += span) {
 
-	float R = 240;
-	for (int r = 10; r <= 70; r += 30) {
+				float noise_step = flag ? 0.045 : 0.01;
+				float noise_value = ofMap(ofNoise(x * 0.01, y * 0.003 + ofGetFrameNum() * noise_step, ofGetFrameNum() * 0.01), 0, 1, 0, 10);
+				if ((int)noise_value % 5 < 4) {
 
-		for (float phi_deg = 0; phi_deg < 360; phi_deg += phi_deg_step) {
+					continue;
+				}
 
-			for (float theta_deg = 0; theta_deg < 360; theta_deg += theta_deg_step) {
+				this->mesh.addVertex(glm::vec3(x, y, 0));
+				this->mesh.addVertex(glm::vec3(x + span, y, 0));
+				this->mesh.addVertex(glm::vec3(x + span, y + span, 0));
+				this->mesh.addVertex(glm::vec3(x, y + span, 0));
 
-				auto noise_value = ofNoise(glm::vec4(this->make_point(R, 30, theta_deg, phi_deg) * 0.0085, r * 0.05 + ofGetFrameNum() * 0.01));
-				if (noise_value <= threshold_1 || noise_value >= threshold_2) { continue; }
+				this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 4); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
+				this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 2); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
 
-				auto noise_1 = ofNoise(glm::vec4(this->make_point(R, 30, theta_deg - theta_deg_step, phi_deg) * 0.0085, r * 0.05 + ofGetFrameNum() * 0.01));
-				auto noise_2 = ofNoise(glm::vec4(this->make_point(R, 30, theta_deg, phi_deg + phi_deg_step) * 0.0085, r * 0.05 + ofGetFrameNum() * 0.01));
-				auto noise_3 = ofNoise(glm::vec4(this->make_point(R, 30, theta_deg, phi_deg - phi_deg_step) * 0.0085, r * 0.05 + ofGetFrameNum() * 0.01));
-				auto noise_4 = ofNoise(glm::vec4(this->make_point(R, 30, theta_deg + theta_deg_step, phi_deg) * 0.0085, r * 0.05 + ofGetFrameNum() * 0.01));
-
-				auto index = this->face.getNumVertices();
-				vector<glm::vec3> vertices;
-
-				vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg - theta_deg_step * 0.5, phi_deg - phi_deg_step * 0.5)));
-				vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg + theta_deg_step * 0.5, phi_deg - phi_deg_step * 0.5)));
-				vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg - theta_deg_step * 0.5, phi_deg + phi_deg_step * 0.5)));
-				vertices.push_back(glm::vec3(this->make_point(R, r, theta_deg + theta_deg_step * 0.5, phi_deg + phi_deg_step * 0.5)));
-
-				this->face.addVertices(vertices);
-
-				color.setHsb(ofMap(r, 10, 100, 0, 255), 130, 255);
+				color.setHsb(ofMap(x_start, 18, 720, 0, 255), 230, 0);
 				for (int i = 0; i < 4; i++) {
 
-					this->face.addColor(ofColor(color, 64));
-				}
-
-				this->face.addIndex(index + 0); this->face.addIndex(index + 1); this->face.addIndex(index + 3);
-				this->face.addIndex(index + 0); this->face.addIndex(index + 3); this->face.addIndex(index + 2);
-
-				if (noise_1 <= threshold_1 || noise_1 >= threshold_2) {
-
-					this->line.addVertex(vertices[0]);
-					this->line.addVertex(vertices[2]);
-
-					this->line.addColor(color);
-					this->line.addColor(color);
-
-					this->line.addIndex(this->line.getNumVertices() - 1);
-					this->line.addIndex(this->line.getNumVertices() - 2);
-				}
-
-				if (noise_2 <= threshold_1 || noise_2 >= threshold_2) {
-
-					this->line.addVertex(vertices[2]);
-					this->line.addVertex(vertices[3]);
-
-					this->line.addColor(color);
-					this->line.addColor(color);
-
-					this->line.addIndex(this->line.getNumVertices() - 1);
-					this->line.addIndex(this->line.getNumVertices() - 2);
-				}
-
-				if (noise_3 <= threshold_1 || noise_3 >= threshold_2) {
-
-					this->line.addVertex(vertices[0]);
-					this->line.addVertex(vertices[1]);
-
-					this->line.addColor(color);
-					this->line.addColor(color);
-
-					this->line.addIndex(this->line.getNumVertices() - 1);
-					this->line.addIndex(this->line.getNumVertices() - 2);
-				}
-
-				if (noise_4 <= threshold_1 || noise_4 >= threshold_2) {
-
-					this->line.addVertex(vertices[1]);
-					this->line.addVertex(vertices[3]);
-
-					this->line.addColor(color);
-					this->line.addColor(color);
-
-					this->line.addIndex(this->line.getNumVertices() - 1);
-					this->line.addIndex(this->line.getNumVertices() - 2);
-
+					this->mesh.addColor(color);
 				}
 			}
 		}
@@ -119,17 +53,11 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-	this->cam.begin();
-	ofRotateY(ofGetFrameNum() * 2.88);
-
-	this->line.draw();
-	this->face.draw();
-
-	this->cam.end();
+	this->mesh.draw();
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
-	int start = 500;
+	int start = 5;
 	if (ofGetFrameNum() > start) {
 
 		std::ostringstream os;
@@ -144,22 +72,6 @@ void ofApp::draw() {
 	}
 	*/
 }
-
-//--------------------------------------------------------------
-glm::vec3 ofApp::make_point(float R, float r, float u, float v) {
-
-	// 数学デッサン教室 描いて楽しむ数学たち　P.31
-
-	u *= DEG_TO_RAD;
-	v *= DEG_TO_RAD;
-
-	auto x = (R + r * cos(u)) * cos(v);
-	auto y = (R + r * cos(u)) * sin(v);
-	auto z = r * sin(u);
-
-	return glm::vec3(x, y, z);
-}
-
 
 //--------------------------------------------------------------
 int main() {
