3e36d65f3980d55e9abba93bc515d0d83f83c75e
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-06-21 20:41:04 +0900

    Gradation ameba. https://junkiyoshi.com/openframeworks20260621/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,7 +6,7 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
+	ofBackground(39);
 }
 
 //--------------------------------------------------------------
@@ -17,17 +17,15 @@ void ofApp::update() {
 	float span = 0.5;
 	ofColor color;
 	bool flag = true;
-	for (float x_start = 0; x_start < 720; x_start += 144)
+	for (float x_start = 0; x_start < 720; x_start += 720)
 	{
-		flag = !flag;
-
-		for (float x = x_start + 7; x < x_start + 137; x += span) {
+		for (float x = x_start; x < 720; x += span) {
 
 			for (float y = 0; y < 720; y += span) {
 
 				float noise_step = flag ? 0.045 : 0.01;
-				float noise_value = ofMap(ofNoise(x * 0.01, y * 0.003 + ofGetFrameNum() * noise_step, ofGetFrameNum() * 0.01), 0, 1, 0, 10);
-				if ((int)noise_value % 5 < 4) {
+				float noise_value = ofMap(ofNoise(x * 0.02, y * 0.02, ofGetFrameNum() * 0.01), 0, 1, 0, 10);
+				if ((int)noise_value != 5) {
 
 					continue;
 				}
@@ -40,7 +38,7 @@ void ofApp::update() {
 				this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 4); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
 				this->mesh.addIndex(this->mesh.getNumVertices() - 1); this->mesh.addIndex(this->mesh.getNumVertices() - 2); this->mesh.addIndex(this->mesh.getNumVertices() - 3);
 
-				color.setHsb(ofMap(x_start, 18, 720, 0, 255), 230, 0);
+				color.setHsb(ofMap(x, 0, 720, 0, 255), 130, 255);
 				for (int i = 0; i < 4; i++) {
 
 					this->mesh.addColor(color);
