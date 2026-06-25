7c02b54f7fc63e397da5af1e1848b0e7ff37a11e
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-06-25 18:57:34 +0900

    Crystal Sea Urchin. https://junkiyoshi.com/openframeworks20260625/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,10 +6,12 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openframeworks");
 
-	ofBackground(239);
+	ofBackground(39);
+	ofSetLineWidth(2);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_ADD);
 	ofEnableDepthTest();
 
-	this->ico_sphere = ofIcoSpherePrimitive(150, 4);
+	this->ico_sphere = ofIcoSpherePrimitive(150, 2);
 	this->noise_value = ofRandom(1000);
 }
 
@@ -22,17 +24,17 @@ void ofApp::update() {
 	for (auto& triangle : triangle_list) {
 
 		glm::vec3 avg = (triangle.getVertex(0) + triangle.getVertex(1) + triangle.getVertex(2)) / 3;
-		auto noise_value = ofNoise(avg.x * 0.03, avg.y * 0.03, avg.z * 0.03, this->noise_value);
+		auto noise_value = ofNoise(avg.x * 0.01, avg.y * 0.01, avg.z * 0.01, this->noise_value);
 
 		float noise_radius = 150;
 		noise_radius = ofMap(noise_value, 0, 1, 50, 450);
 
 		int index = this->mesh.getNumVertices();
 
-		this->mesh.addVertex(glm::normalize(avg) * (noise_radius + 100));
-		this->mesh.addVertex(glm::normalize(triangle.getVertex(0)) * noise_radius);
-		this->mesh.addVertex(glm::normalize(triangle.getVertex(1)) * noise_radius);
-		this->mesh.addVertex(glm::normalize(triangle.getVertex(2)) * noise_radius);
+		this->mesh.addVertex(glm::normalize(avg) * noise_radius);
+		this->mesh.addVertex(triangle.getVertex(0));
+		this->mesh.addVertex(triangle.getVertex(1));
+		this->mesh.addVertex(triangle.getVertex(2));
 
 		this->mesh.addIndex(index + 0);
 		this->mesh.addIndex(index + 1);
@@ -58,20 +60,18 @@ void ofApp::update() {
 void ofApp::draw() {
 
 	this->cam.begin();
-	ofRotateX(ofGetFrameNum() * 0.43);
-	ofRotateY(ofGetFrameNum() * 0.65);
 
-	ofSetColor(39);
+	ofSetColor(39, 39, 255, 128);
 	this->mesh.drawFaces();
 
-	ofSetColor(239);
+	ofSetColor(255);
 	this->mesh.drawWireframe();
 
 	this->cam.end();
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
-	int start = 250;
+	int start = 500;
 	if (ofGetFrameNum() > start) {
 
 		std::ostringstream os;
