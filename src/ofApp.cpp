a53623ac05f67f10ddceed64da6226d9b612ed15
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-08-22 21:20:28 +0900

    Gradation Group. https://junkiyoshi.com/openframeworks20260822/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,7 +6,7 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
+	ofBackground(39);
 	ofSetLineWidth(2);
 }
 
@@ -22,39 +22,42 @@ void ofApp::draw() {
 	ofTranslate(ofGetWindowSize() * 0.5);
 
 	auto radius = 250;
-	for (int k = 0; k < 75; k++) {
+	ofColor color;
+	for (int k = 0; k < 180; k++) {
 
 		auto noise_seed = glm::vec3(ofRandom(1000), ofRandom(1000), ofRandom(1000));
-		auto max_size = ofMap(k, 0, 75, 5, 10);
+		auto max_size = ofMap(k, 0, 180, 12, 12);
 
-		ofSetColor(0);
-		for (int i = 0; i < 15; i++) {
+		ofSetColor(239);
+		for (int i = 0; i < 25; i++) {
 
-			auto deg = (ofGetFrameNum() + i) * 1.44 + k * 4;
+			auto deg = (ofGetFrameNum() + i) * 0.36 + k * 2;
 			auto base_location = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
 
 			auto location = glm::vec2(
-				ofMap(ofNoise(noise_seed.x, (k + i + ofGetFrameNum()) * 0.035), 0, 1, -40, 40),
-				ofMap(ofNoise(noise_seed.y, (k + i + ofGetFrameNum()) * 0.035), 0, 1, -40, 40));
+				ofMap(ofNoise(noise_seed.x, k + ofGetFrameNum() * 0.035 + i * 0.03), 0, 1, -40, 40),
+				ofMap(ofNoise(noise_seed.y, k + ofGetFrameNum() * 0.035 + i * 0.03), 0, 1, -40, 40));
 
 			location += base_location;
-			auto size = ofMap(i, 0, 15, 3, max_size + 3); 
+			auto size = ofMap(i, 0, 25, 3, max_size + 3); 
 
 			ofDrawCircle(location, size);
 		}
 
-		ofSetColor(255);
-		for (int i = 0; i < 15; i++) {
+		color.setHsb(ofMap(k, 0, 180, 0, 255), 255, 255);
 
-			auto deg = (ofGetFrameNum() + i) * 1.44 + k * 4;
+		ofSetColor(color);
+		for (int i = 0; i < 25; i++) {
+
+			auto deg = (ofGetFrameNum() + i) * 0.36 + k * 2;
 			auto base_location = glm::vec2(radius * cos(deg * DEG_TO_RAD), radius * sin(deg * DEG_TO_RAD));
 
 			auto location = glm::vec2(
-				ofMap(ofNoise(noise_seed.x, (k + i + ofGetFrameNum()) * 0.035), 0, 1, -40, 40),
-				ofMap(ofNoise(noise_seed.y, (k + i + ofGetFrameNum()) * 0.035), 0, 1, -40, 40));
+				ofMap(ofNoise(noise_seed.x, k + ofGetFrameNum() * 0.035 + i * 0.03), 0, 1, -40, 40),
+				ofMap(ofNoise(noise_seed.y, k + ofGetFrameNum() * 0.035 + i * 0.03), 0, 1, -40, 40));
 
 			location += base_location;
-			auto size = ofMap(i, 0, 15, 1, max_size);
+			auto size = ofMap(i, 0, 25, 1, max_size);
 
 			ofDrawCircle(location, size);
 		}
@@ -62,7 +65,7 @@ void ofApp::draw() {
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
-	int start = 30;
+	int start = 770;
 	if (ofGetFrameNum() > start) {
 
 		std::ostringstream os;
